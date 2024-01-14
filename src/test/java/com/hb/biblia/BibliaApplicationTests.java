0d13f4c58e299b378544fad6628e1afa8e927c7a
BernardoMendess <bernardo_mendes@id.uff.br>
2024-01-13 21:01:59 -0300

    comecando o projeto

---
+++ src/test/java/com/hb/biblia/BibliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hb.biblia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
